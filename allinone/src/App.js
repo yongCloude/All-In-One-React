96821c737ab0be32fe1a5ad2c529d1630b9c95b1
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-30 08:27:10 +0900

    [feat] 홈, 로그인, 회원가입, 게시판 router 설정

--- allinone/src/App.js
+++ allinone/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, Routes } from '../node_modules/react-router-dom/index';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import Home from './pages/Home';
+import PostListPage from './pages/Post/PostListPage';
+import PostWritePage from './pages/Post/PostWritePage';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/write' element={<PostWritePage/>}/>
+        <Route path='/posts' element={<PostListPage/>}/>
+        <Route path='/post/:postId' element={<RegisterPage/>}/>
+      </Routes>
+    </>
   );
 };
 
