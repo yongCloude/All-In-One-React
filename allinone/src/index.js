ef0e52ceb9da032ed9438f99ec46f6bf4746ad6c
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-08-30 08:52:38 +0900

    [feat] redux 최초 설정

--- allinone/src/index.js
+++ allinone/src/index.js
@@ -5,12 +5,31 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from '../node_modules/react-router-dom/index';
 
+import rootReducer from './modules/index';
+import { Provider } from '../node_modules/react-redux/es/exports';
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+
+
+
+
+
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: composeWithDevTools,
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
