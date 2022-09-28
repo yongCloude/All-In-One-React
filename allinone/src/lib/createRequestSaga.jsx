908fb2eecbd3b12528cef4a0a9e8a6be426d066a
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-09-28 19:25:57 +0900

    [feat] redux saga creator

---
+++ allinone/src/lib/createRequestSaga.jsx
@@ -0,0 +1,59 @@
+import {call, put} from 'redux-saga/effects';
+import { finishLoading, startLoading } from '../modules/loading';
+
+
+export const createRequestActionTypes = type => {
+    const SUCCESS = `${type}_SUCCESS`;
+    const FAILURE = `${type}_FAILURE`;
+    return [type, SUCCESS, FAILURE];
+}
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type)); // 로딩 시작
+    try {
+      const response = yield call(request, action.payload);
+      console.log(response);
+      yield put({
+        type: SUCCESS,
+        payload: response.data.data,
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(finishLoading(type)); // 로딩 끝
+  };
+}
+
+export function createRequestSagaReturnSuccess(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type)); // 로딩 시작
+    try {
+      
+      const response = yield call(request, action.payload);
+      
+      yield put({
+        type: SUCCESS,
+        payload: response,
+      });
+      
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(finishLoading(type)); // 로딩 끝
+  };
+}
