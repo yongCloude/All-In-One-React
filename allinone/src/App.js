9c5a5d9f2aad1502e9511f4fe545759c658c561c
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-09-28 19:27:04 +0900

    [feat] Route 처리

--- allinone/src/App.js
+++ allinone/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route, Routes } from '../node_modules/react-router-dom/index';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
+import ChatListPage from './pages/chat/ChatListPage';
 import Home from './pages/Home';
 import PostListPage from './pages/Post/PostListPage';
+import PostViewPage from './pages/Post/PostViewPage';
 import PostWritePage from './pages/Post/PostWritePage';
 
 const App = () => {
@@ -15,7 +17,8 @@ const App = () => {
         <Route path='/register' element={<RegisterPage/>}/>
         <Route path='/write' element={<PostWritePage/>}/>
         <Route path='/posts' element={<PostListPage/>}/>
-        <Route path='/post/:postId' element={<RegisterPage/>}/>
+        <Route path='/chat' element={<ChatListPage/>}/>
+        <Route path='/post/:postId' element={<PostViewPage/>}/>
       </Routes>
     </>
   );
