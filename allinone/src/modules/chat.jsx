c439eceded700eb766eda018e743ebdcb5b0631e
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-10-13 14:26:39 +0900

    [module] 채팅방 불러오기, 나가기, 내가 만든 채팅방 불러오기

--- allinone/src/modules/chat.jsx
+++ allinone/src/modules/chat.jsx
@@ -11,18 +11,48 @@ const [
     LIST_CHATS_FAILURE,
 ] = createRequestActionTypes('chat/LIST_CHATS');
 
+const [
+    LIST_MY_ROOMS,
+    LIST_MY_ROOMS_SUCCESS,
+    LIST_MY_ROOMS_FAILURE,
+] = createRequestActionTypes('chat/LIST_MY_ROOMS');
+
+const [
+    LIST_ROOMS,
+    LIST_ROOMS_SUCCESS,
+    LIST_ROOMS_FAILURE,
+] = createRequestActionTypes('chat/LIST_ROOMS');
+
+const [
+    EXIT_ROOM,
+    EXIT_ROOM_SUCCESS,
+    EXIT_ROOM_FAILURE,
+] = createRequestActionTypes('chat/EXIT_ROOM');
+
+
 const GET_CHAT = 'chat/GET_CHAT';
 
-export const listChats = createAction(LIST_CHATS, token => token);
+export const listChats = createAction(LIST_CHATS, ({token, channel_id}) => ({token, channel_id}));
 export const getChat = createAction(GET_CHAT);
 
-const readChatSaga = createRequestSagaReturnSuccess(LIST_CHATS, postAPI.readChat)
+export const listRooms = createAction(LIST_ROOMS);
+export const listMyRooms = createAction(LIST_MY_ROOMS, token => token); 
+export const exitRoom = createAction(EXIT_ROOM, ({token, channel_id}) => ({token, channel_id}));
+
+const readChatSaga = createRequestSaga(LIST_CHATS, postAPI.readChat);
+const getRoomSaga = createRequestSagaReturnSuccess(LIST_ROOMS, postAPI.getRooms);
+const getMyRoomSaga = createRequestSagaReturnSuccess(LIST_MY_ROOMS, postAPI.getMyRooms);
+const exitRoomSaga = createRequestSagaReturnSuccess(EXIT_ROOM, postAPI.exitRoom);
 export function* chatSaga(){
     yield takeLatest(LIST_CHATS, readChatSaga);
+    yield takeLatest(LIST_ROOMS, getRoomSaga);
+    yield takeLatest(LIST_MY_ROOMS, getMyRoomSaga);
+    yield takeLatest(EXIT_ROOM, exitRoomSaga);
 }
 
 const initialState = {
     messages: null,
+    rooms: null,
     error: null,
 };
 
@@ -30,7 +60,7 @@ const chat = handleActions(
     {
         [LIST_CHATS_SUCCESS]: (state, {payload: messages}) => ({
             ...state,
-            messages: messages.data
+            messages: messages.records
         }),
         [LIST_CHATS_FAILURE]: (state, {payload: error}) => ({
             ...state,
@@ -40,6 +70,29 @@ const chat = handleActions(
             ...state,
             messages: state.messages.concat(message)
         }),
+        [LIST_ROOMS_SUCCESS]: (state, {payload: rooms}) => ({
+            ...state,
+            rooms: rooms.data
+        }),
+        [LIST_ROOMS_FAILURE]: (state, {payload:error}) => ({
+            ...state,
+            error,
+        }),
+        [LIST_MY_ROOMS_SUCCESS]: (state, {payload: rooms}) => ({
+            ...state,
+            rooms: rooms.data
+        }),
+        [LIST_MY_ROOMS_FAILURE]: (state, {payload:error}) => ({
+            ...state,
+            error,
+        }), 
+        [EXIT_ROOM_SUCCESS]: (state, {payload: isSuccess}) => ({
+            ...state,
+        }),
+        [EXIT_ROOM_FAILURE]: (state, {payload:error}) => ({
+            ...state,
+            error,
+        }), 
     },
     initialState,
 );
