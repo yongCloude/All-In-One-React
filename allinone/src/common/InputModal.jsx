35122bc08444089fbcb991219394cc8f52dfa99f
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-10-13 14:23:16 +0900

    [component] 채팅방 생성 및 검색

---
+++ allinone/src/common/InputModal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from './Button';
+import '../styles/InputModal.scss';
+const InputModal = ({
+  visible,
+  title,
+  description,
+  onChange,
+  onConfirm,
+  onCancel,
+  cancelText = '취소',
+  confirmText = '확인',
+}) => {
+    if(!visible) return null;
+  return (
+    <div className="FullScreen">
+      <div className="InputModal">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <input type="text" onChange={onChange}/>
+        <div className="Buttons">
+          <Button cyan={false} onClick={onCancel}>
+            {cancelText}
+          </Button>
+          <Button cyan={false} onClick={onConfirm}>
+            {confirmText}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InputModal;
--- allinone/src/common/SearchBar.jsx
+++ allinone/src/common/SearchBar.jsx
@@ -1,31 +1,13 @@
 
 
 import React from 'react';
-import Select from 'react-select';
 import '../styles/SearchBar.scss';
 import Button from './Button';
-const SearchBar = ({option, onChange, setOption, onClick}) => {
+const SearchBar = ({option, onChange, onClick}) => {
     return (
         <div className='SearchBar'>
             <input value={option} onChange={onChange} name="title"/>
-        <Select
-          onChange={(e) => setOption(e.value)}
-          placeholder="옵션선택"
-          options={[
-            {
-              value: "title",
-              label: "title"
-            },
-            {
-              value: "writer",
-              label: "writer"
-            },
-            {
-              value: "all",
-              label: "all"
-            },
-          ]}
-        />
+        
         <Button onClick={onClick}>검색</Button>
         
         </div>
