990c48663e1a2e4745c81da4203aff99a809160c
yongun2 <djkye0001@naver.com>
2022-11-13 20:55:19 +0900

    [ui] header ui bootstrap 적용

--- allinone/src/common/Header.jsx
+++ allinone/src/common/Header.jsx
@@ -1,48 +1,35 @@
-import React from 'react';
-import Button from './Button';
-import '../styles/common/Header.scss';
-import Responsive from './Responsive';
-import { useNavigate } from '../../node_modules/react-router-dom/index';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = ({user, onLogout}) => {
-
-  const navigate = useNavigate();
-
+function Header({ user, onLogout }) {
 
   return (
-    <div>
-      <div className="Header">
-        <Responsive>
-          <div className="Wrapper">
-            <div id="logo" onClick={()=>navigate('/')}>AllInOne</div>
-            <div id="center">
-                <Button to="/posts">게시판</Button>
-                <Button to="/chat">채팅</Button>
-                {user && <Button to="/mychat">나의 채팅방</Button>}
-                {user && <Button to="/mypage">마이 페이지</Button>}
-                
-            </div>
-            {user ? (
-              <div id="right">
-                <div className="UserInfo">{user.name}</div>  
-                <Button onClick={onLogout}>로그아웃</Button>
-              </div>
-            ) : (
-              <div id="right">  
-              <Button to="/login">로그인</Button>
-            </div>
-            )}
-            
-          </div>
-        </Responsive>
-      </div>
-      <Spacer />
-    </div>
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href='#home'>AllInOne</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='cafe-map'>카페 찾기</Nav.Link>
+            <Nav.Link href='/posts'>게시판</Nav.Link>
+            <NavDropdown title='채팅' id='collasible-nav-dropdown'>
+              <NavDropdown.Item href='/chat'>모든 채팅방</NavDropdown.Item>
+              {user && (<NavDropdown.Item href='/mychat'>내가 만든 채팅방</NavDropdown.Item>)}
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href='/mypage'>마이 페이지</Nav.Link>
+            {user == null ?
+              (<Nav.Link eventKey={2} href='/login'>로그인</Nav.Link>) :
+              (<Nav.Link eventKey={2} onClick={onLogout}>로그아웃</Nav.Link>)
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
-
-const Spacer = () => {
-  return <div className="Spacer"></div>;
-};
+}
 
 export default Header;
