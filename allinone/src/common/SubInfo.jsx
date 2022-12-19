b5f8850388da499257f56d88f839bd6b59120a11
yongun2 <djkye0001@naver.com>
2022-12-19 16:17:34 +0900

    [fix] invalid date 버그 수정

--- allinone/src/common/SubInfo.jsx
+++ allinone/src/common/SubInfo.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import '../styles/common/SubInfo.scss';
 
 
-const SubInfo = ({username, publishDate, likes, views}) => {
-    return (
-        <div className='SubInfo'>
+const SubInfo = ({ username, publishDate, likes, views }) => {
+
+  const [ year, month, date ] = publishDate;
+
+  return (
+    <div className='SubInfo'>
             <span>
                 <b>
                     {username}
                 </b>
             </span>
-            <span>{new Date(publishDate).toLocaleDateString()}</span>
-            <span className="Etc">
+      <span>{year}년 {month}월 {date}일</span>
+      <span className='Etc'>
                 <span>좋아요 {likes}</span>
                 <span>조회수 {views}회</span>
             </span>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default SubInfo;
