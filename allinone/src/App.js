c92c26266dce073218391d2a8e81af7dff5bcfe2
yongun2 <djkye0001@naver.com>
2023-01-05 13:47:36 +0900

    [update] 헤더 링크 수정

--- allinone/src/App.js
+++ allinone/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Route, Routes } from '../node_modules/react-router-dom/index';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
-import ChatListPage from './pages/chat/ChatListPage';
-import ChatRoomPage from './pages/chat/ChatRoomPage';
-import MyChatListPage from './pages/chat/MyChatListPage';
+
 import Home from './pages/Home';
 import PostListPage from './pages/Post/PostListPage';
 import PostViewPage from './pages/Post/PostViewPage';
@@ -12,6 +10,7 @@ import PostWritePage from './pages/Post/PostWritePage';
 import MyPage from './pages/auth/MyPage';
 import CafeMainPage from './pages/cafe/CafeMainPage';
 import CafeDetailPage from './pages/cafe/CafeDetailPage';
+import ChatRoomPage from './pages/chat/ChatRoomPage';
 
 const App = () => {
   return (
@@ -22,12 +21,10 @@ const App = () => {
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/write' element={<PostWritePage />} />
         <Route path='/posts' element={<PostListPage />} />
-        <Route path='/chat' element={<ChatListPage />} />
+        <Route path='/chat/*' element={<ChatRoomPage />} />
         <Route path='/cafe' element={<CafeMainPage />} />
         <Route path='/cafe/detail/:cafe_id' element={<CafeDetailPage />} />
-        <Route path='/mychat' element={<MyChatListPage />} />
         <Route path='/mypage/*' element={<MyPage />}></Route>
-        <Route path='/chat/:channelId' element={<ChatRoomPage />} />
         <Route path='/post/:postId' element={<PostViewPage />} />
       </Routes>
     </>
